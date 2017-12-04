96c6683b77b713013ae1777b463d3c240c5fd505
usernamen7 <n7username7@gmail.com>
2017-12-04 16:21:25 +0200

    fix layout, input by enter

--- src/app/classes/todo.ts
+++ src/app/classes/todo.ts
@@ -1,9 +1,11 @@
 export class Todo {
   id: number;
   text: string;
+  // comment: string;
 
-  constructor(id: number, text: string) {
+  constructor(id: number, text: string /*, comment: string */) {
     this.id = id;
     this.text = text;
+    // this.comment = comment;
   };
 }
